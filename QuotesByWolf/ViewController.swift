ce2af8a8526a42bce60c69eeeeee5cf69bd648ca
KEKSman <bassman7009@gmail.com>
2020-10-16 14:37:35 +0500

    add icons

--- QuotesByWolf/ViewController.swift
+++ QuotesByWolf/ViewController.swift
@@ -1,19 +1,45 @@
-//
-//  ViewController.swift
-//  QuotesByWolf
-//
-//  Created by Евгений колесников on 16.10.2020.
-//
-
 import UIKit
+import AVFoundation
+
+private let imageUrl = "https://picsum.photos/200"
 
 class ViewController: UIViewController {
-
+    @IBOutlet var button: UIButton!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func actionButton(_ sender: UIButton) {
+        fetchImage()
+        currentQuote()
+    }
+    
+    private func fetchImage(){
+        guard let url = URL(string: imageUrl) else { return }
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if  let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let response = response {
+                print(response)
+            }
+            if let data = data, let image = UIImage(data: data){
+                DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
+        }.resume()
+    }
+    private func currentQuote(){
+        messageLabel.text = quotes.randomElement()
+        let synthesizer = AVSpeechSynthesizer()
+        let utterance = AVSpeechUtterance(string: messageLabel.text!)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ru-RU")
+        synthesizer.speak(utterance)
+    }
 }
 
